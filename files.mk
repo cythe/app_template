3f7a376c630690580073883c92936015842ef66c
cythe <harrylighter@hotmail.com>
2024-05-12 16:34:03 +0800

    file: merge files.mk to rules.mk
    There's no need to separate these two files, merge them.
    
    Signed-off-by: cythe <harrylighter@hotmail.com>

--- files.mk
+++
@@ -1,9 +0,0 @@
-inc_files := $(wildcard *.h)
-c_srcs  := $(wildcard *.c)
-cx_srcs := $(wildcard *.cpp)
-
-c_deps 	:= $(patsubst %c, %d, $(c_srcs))
-cx_deps := $(patsubst %cpp, %dx, $(cx_srcs))
-
-c_objs  := $(patsubst %c, %o, $(c_srcs))
-cx_objs := $(patsubst %cpp, %ox, $(cx_srcs))
